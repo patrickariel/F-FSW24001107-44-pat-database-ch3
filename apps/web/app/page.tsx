2b3e27dc68c8b747711bff6cc43f854de0c18e54
patrickariel <161032380+patrickariel@users.noreply.github.com>
2024-05-25 19:34:53 +0700

    style: tree-wide reformat

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,8 +1,8 @@
-import Image from "next/image";
-import { Card } from "@repo/ui/card";
-import { Code } from "@repo/ui/code";
 import styles from "./page.module.css";
 import { Button } from "@repo/ui/button";
+import { Card } from "@repo/ui/card";
+import { Code } from "@repo/ui/code";
+import Image from "next/image";
 
 function Gradient({
   conic,
