b382b024d07fd2a01842e0cb5a7fdf1c2dc245c5
patrickariel <161032380+patrickariel@users.noreply.github.com>
2024-05-28 01:08:48 +0700

    refactor(ui,eslint-config): improve linting rules

--- packages/eslint-config/react-internal.js
+++ packages/eslint-config/react-internal.js
@@ -10,7 +10,13 @@ const project = resolve(process.cwd(), "tsconfig.json");
 
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ["eslint:recommended", "prettier", "eslint-config-turbo"],
+  extends: [
+    "eslint:recommended",
+    "prettier",
+    "eslint-config-turbo",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/stylistic",
+  ],
   plugins: ["only-warn"],
   env: {
     browser: true,
