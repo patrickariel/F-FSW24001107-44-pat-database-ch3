7eb904e737fd533565b66a47c5c1afb1e2d6222c
patrickariel <161032380+patrickariel@users.noreply.github.com>
2024-05-27 20:36:11 +0700

    feat(api): export types for inferring router's inputs/outputs

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,6 +1,7 @@
 import { product } from "./routes/product";
 import { user } from "./routes/user";
 import { createCallerFactory, router } from "./trpc";
+import { inferRouterInputs, inferRouterOutputs } from "@trpc/server";
 
 export const appRouter = router({
   product,
@@ -10,3 +11,5 @@ export const appRouter = router({
 export const createCaller = createCallerFactory(appRouter);
 
 export type AppRouter = typeof appRouter;
+export type RouterInput = inferRouterInputs<AppRouter>;
+export type RouterOutput = inferRouterOutputs<AppRouter>;
