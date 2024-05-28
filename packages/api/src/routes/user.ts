537d6cb4bf0f4e35ea0be2fe4b0596dfd45d6af0
patrickariel <161032380+patrickariel@users.noreply.github.com>
2024-05-28 15:25:05 +0700

    refactor(api): reduce redundant database queries

--- packages/api/src/routes/user.ts
+++ packages/api/src/routes/user.ts
@@ -5,30 +5,7 @@ import _ from "lodash";
 import { z } from "zod";
 
 export const user = router({
-  get: userProcedure
-    .input(z.object({ id: z.string().uuid() }))
-    .output(
-      z.object({
-        id: z.string(),
-      }),
-    )
-    .query(async ({ input: { id } }) => {
-      const user = await db.user.findUnique({
-        where: { id },
-      });
-      if (user === null) {
-        throw new TRPCError({
-          code: "NOT_FOUND",
-          message: "No such user in the database",
-        });
-      }
-      return _.omit(user, "password");
-    }),
-  getByEmail: userProcedure
-    .input(z.object({ email: z.string().email() }))
-    .query(async ({ input: { email } }) =>
-      _.omit(db.user.findUnique({ where: { email } }), "password"),
-    ),
+  get: userProcedure.query(({ ctx: { user } }) => user),
   update: userProcedure
     .input(
       z.object({
