19620c966d8e27065189c5b91ece76a539f11b5a
patrickariel <161032380+patrickariel@users.noreply.github.com>
2024-05-29 19:23:15 +0700

    build(server): move entry point configuration into the config file

--- apps/server/tsup.config.ts
+++ apps/server/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, type Options } from "tsup";
 
 export default defineConfig((options: Options) => ({
-  entryPoints: ["src/index.ts"],
+  entry: ["app/index.ts"],
   clean: true,
   format: ["esm"],
   external: [/^@prisma\/.*/],
