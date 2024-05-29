bd7de030c32717701064f38a9cc62c75ffadfe9a
patrickariel <161032380+patrickariel@users.noreply.github.com>
2024-05-29 19:20:52 +0700

    build(server): output as ESM
    We need to bundle our monorepo dependencies so that they are transpiled.
    But Prisma still needs to be externalized because they are CJS-only

--- apps/server/tsup.config.ts
+++ apps/server/tsup.config.ts
@@ -3,7 +3,8 @@ import { defineConfig, type Options } from "tsup";
 export default defineConfig((options: Options) => ({
   entryPoints: ["src/index.ts"],
   clean: true,
-  format: ["cjs"],
-  noExternal: [/.*/],
+  format: ["esm"],
+  external: [/^@prisma\/.*/],
+  noExternal: [/^@repo\/.*/],
   ...options,
 }));
